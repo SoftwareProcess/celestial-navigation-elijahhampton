021ce5faa1449f509a7bc103dcb56307eb7d6482
elijahhampton <elijahhampton880@gmail.com>
2019-02-20 14:02:41 -0600

    Green Light |

--- nav/adjust.py
+++ nav/adjust.py
@@ -1,8 +1,13 @@
 def adjust(values = None):
     #check instances
+    hasWrongType = False
     for x in values:
         if (not(isinstance(values[x], basestring))):
-            values['error'] = 'error'
+            hasWrongType = True
+    
+    if (hasWrongType is True):
+        values['error'] = 'error'
+        
         
         values['altitude'] = '1'
     return values
