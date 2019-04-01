aac7248798e8dba0b6675c8eee734e7c490fc055
elijahhampton <ejh0017@gmail.com>
2019-04-01 12:09:12 -0700

    Red Light |

--- nav/correct.py
+++ nav/correct.py
@@ -5,14 +5,14 @@ Created on April 1, 2019
 """
 
 def correct(values = None):
-    #Check if lat is in appropriate bounds
-    if (values['lat'] == '90d0.0'):
-        values['error'] = 'invalid lat'
-        return values
-    
     #Check if lat key is present
     if not("lat" in values):
         values["error"] = "lat parm not present in input dict."
+        return values
+    
+    #Check if lat is in appropriate bounds
+    if (values['lat'] == '90d0.0'):
+        values['error'] = 'invalid lat'
         return values
     
     return values
