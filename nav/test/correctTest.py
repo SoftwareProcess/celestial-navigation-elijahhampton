53fba0649002dfc822026748a00494194b9346bd
elijahhampton <ejh0017@gmail.com>
2019-04-02 16:41:10 -0700

    Green Light |

--- nav/test/correctTest.py
+++ nav/test/correctTest.py
@@ -145,5 +145,4 @@ class corrrectTest(unittest.TestCase):
         result = self.microservice()
         resultDictionary = self.string2dict(result)
         print resultDictionary
-        
         self.assertTrue(resultDictionary.has_key("error"), True)
