dbdca19ed065dcb1fd7d7f004f657ae043b5bd9b
elijahhampton <ejh0017@gmail.com>
2019-04-02 17:23:18 -0700

    Green Light |

--- nav/test/correctTest.py
+++ nav/test/correctTest.py
@@ -185,3 +185,19 @@ class corrrectTest(unittest.TestCase):
                           'error': 'assumedLat parm outside correct boundary.'}
         
         self.assertEqual(correct(self.inputDictionary), tempResultDict)
+        
+    def test200_010ShouldReturnWithErrorKeyIfAssumedLatParmNotPresent(self):
+        self.setParm('lat', '16d32.3')
+        self.setParm('long', '95d41.6')
+        self.setParm('altitude', '13d42.3')
+        self.setParm('assumedLong', '350d35.3')
+        self.setParm('op', 'correct')
+         
+        tempResultDict = {'op': 'correct',
+                          'lat': '16d32.3',
+                          'long': '95d41.6',
+                          'altitude': '13d42.3',
+                          'assumedLong': '350d35.3',
+                          'error': 'assumedLat parm not present.'}
+        
+        self.assertEqual(correct(self.inputDictionary), tempResultDict)
