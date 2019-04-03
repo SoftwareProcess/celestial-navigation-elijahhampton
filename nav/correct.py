e730f9c3764e414ba61f8d0df6caa73c7db1daf5
elijahhampton <ejh0017@gmail.com>
2019-04-03 09:10:52 -0700

    Green Light |

--- nav/correct.py
+++ nav/correct.py
@@ -137,12 +137,11 @@ def correct(values = None):
     round(correctedDistance)
     
     #Determine compass direction in which to make the distance adjustment
-    try:
-        preCorrectedAzimuth =  str( radiansToDegrees( acos(
-            (sin(int(values['lat'].split('d')[0])) + sin(float(values['lat'].split('d')[1]))) - (  (sin(int(values['assumedLat'].split('d')[0])) + sin(float(values['assumedLat'].split('d')[1]))) * intermmediateDistance)
-                / (  (cos(int(values['assumedLat'].split('d')[0])) + cos(float(values['assumedLat'].split('d')[1]))) * cos(correctedDistance))))) #Should be 82.9490446
-    except Exception as ex:
-        print ex
+    print sin(int(values['lat'].split('d')[0])
+    preCorrectedAzimuth =  str( radiansToDegrees( acos(
+        (sin(int(values['lat'].split('d')[0])) + sin(float(values['lat'].split('d')[1]))) - (  (sin(int(values['assumedLat'].split('d')[0])) + sin(float(values['assumedLat'].split('d')[1]))) * intermmediateDistance)
+            / (  (cos(int(values['assumedLat'].split('d')[0])) + cos(float(values['assumedLat'].split('d')[1]))) * cos(correctedDistance))))) #Should be 82.9490446
+    
     
     correctedAzimuthX = int(preCorrectedAzimuth.split('.')[0])
     correctedAzimuthY = float(preCorrectedAzimuth.split('.')[1]) * 60
