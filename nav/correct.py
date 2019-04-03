43ce11f321e4f2b4cb4926ddfe7dcf8a9bd4c878
elijahhampton <ejh0017@gmail.com>
2019-04-03 11:44:23 -0700

    Green Light |

--- nav/correct.py
+++ nav/correct.py
@@ -114,8 +114,9 @@ def correct(values = None):
         return values
     
     #Calculate local hour angle
-    localHourAngleX = int(values['long'].split('d')[0]) + int(values['assumedLong'].split('d')[0])
+    localHourAngleX = (int(values['long'].split('d')[0]) + int(values['assumedLong'].split('d')[0]))
     localHourAngleY = ((float(values['long'].split('d')[1]) + float(values['assumedLong'].split('d')[1])) % 60)
+    localHourAngleX = (int(values['long'].split('d')[0]) + int(values['assumedLong'].split('d')[0])) + (localHourAngleY / 60)
     print(localHourAngleX)
     print(localHourAngleY)
     print(" ")
