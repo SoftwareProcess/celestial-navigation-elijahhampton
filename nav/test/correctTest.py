8d82f33f8aea4cd8e92bf6ec0c07dbbbedd9cf46
elijahhampton <ejh0017@gmail.com>
2019-04-02 17:11:53 -0700

    Green Light |

--- nav/test/correctTest.py
+++ nav/test/correctTest.py
@@ -151,3 +151,21 @@ class corrrectTest(unittest.TestCase):
                           'error': 'altitude parm outside correct boundary.'}
         
         self.assertEqual(correct(self.inputDictionary), tempResultDict)
+        
+    def test200_010ShouldReturnWithErrorKeyIfAltitudeIsNotPresent(self):
+        self.setParm('lat', '16d32.3')
+        self.setParm('long', '95d41.6')
+        self.setParm('altitude', '13d42.3')
+        self.setParm('assumedLat', '53d38.4')
+        self.setParm('assumedLong', '350d35.3')
+        self.setParm('op', 'correct')
+         
+        tempResultDict = {'op': 'correct',
+                          'lat': '16d32.3',
+                          'long': '95d41.6',
+                          'altitude': '13d42.3',
+                          'assumedLat': '53d38.4',
+                          'assumedLong': '350d35.3',
+                          'error': 'altitude parm not present.'}
+        
+        self.assertEqual(correct(self.inputDictionary), tempResultDict)
