391d4da18a9a429ee297a8968800a33c8bfbccc0
elijahhampton <ejh0017@gmail.com>
2019-04-03 20:06:02 -0700

    Green Light |

--- nav/test/correctTest.py
+++ nav/test/correctTest.py
@@ -351,19 +351,20 @@ class corrrectTest(unittest.TestCase):
         self.assertEqual(correct(self.inputDictionary), tempResultDict)
         
     def test200_010ShouldReturnAppropriateErrorIfResultDictContainsANonString(self):
-        self.setParm('lat', '16d32.3')
+        self.setParm('lat', 16d32.3)
         self.setParm('long', '95d41.6')
         self.setParm('altitude', '13d42.3')
         self.setParm('assumedLat', '53d38.4')
-        self.setParm('assumedLong', '350d35.3a')
+        self.setParm('assumedLong', '350d35.3')
         self.setParm('op', 'correct')
         
         tempResultDict = {'op': 'correct',
-                          'lat': '16d32.3',
+                          'lat': 
+        16d32.3,
                           'long': '95d41.6',
                           'altitude': '13d42.3',
                           'assumedLat': '53d38.4',
-                          'assumedLong': '350d35.3a',
+                          'assumedLong': '350d35.3',
                           'error': 'Result dict contains non string key or value.'}
         
         self.assertEqual(correct(self.inputDictionary), tempResultDict)
