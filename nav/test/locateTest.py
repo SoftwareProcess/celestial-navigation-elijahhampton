2ad54018728e8bda3e24b6f097aa9a767e7c0367
elijahhampton <ejh0017@gmail.com>
2019-04-24 13:57:01 -0700

    Red Light |

---
+++ nav/test/locateTest.py
@@ -0,0 +1,41 @@
+""" 
+Created on March 04, 2019
+
+@author Elijah Hampton
+"""
+
+import unittest
+from nav.locate import locate
+from jinja2.utils import missing
+
+class locateTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+#     ----------------------------------------------------------------------
+
+#         self.setParm('assumedLat', '-53d38.4')
+#         self.setParm('assumedLong', '350d35.3')
+#         self.setParm('corrections', '[[100,1d0.0]]')
+#         self.setParm('op', 'locate')
+
+# Happy Path Test
+
+
+# Sad Path Test
+def test200_010ShouldReturnWithErrorKeyIfXOfAssumedLatIsGTNegNinety(self):
+        self.setParm('assumedLat', '-91d38.4')
+        self.setParm('assumedLong', '350d35.3')
+        self.setParm('corrections', '[[100,1d0.0]]')
+        self.setParm('op', 'locate')
+          
+        tempResultDict = {'op': 'locate',
+                          'assumedLat': '-53d38.4',
+                          'assumedLong': '350d35.3',
+                          'error': 'assumedLat parm outside correct boundary.'}
+         
+        self.assertEqual(locate(self.inputDictionary), tempResultDict)
