4f76f88cca9aef0b1490df1b473e972bcc628c18
elijahhampton <ejh0017@gmail.com>
2019-04-29 11:52:37 -0700

    Green Light |

--- nav/locate.py
+++ nav/locate.py
@@ -1,4 +1,4 @@
 def locate(values = None):
-    if (values['assumedLat'] > -90):
+    if (values['assumedLat'] < -90):
         values['error']= 'assumedLat parm outside correct boundary.'
     return values
