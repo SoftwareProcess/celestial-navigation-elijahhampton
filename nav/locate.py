0473cd43f84b69f86bc977bca8f551b7239d4ea3
elijahhampton <ejh0017@gmail.com>
2019-04-29 12:03:59 -0700

    Red Light |

--- nav/locate.py
+++ nav/locate.py
@@ -2,7 +2,7 @@ def locate(values = None):
     if (int(values['assumedLat'].split('d')[0]) < -90 or int(values['assumedLat'].split('d')[0]) > 90):
         values['error']= 'assumedLat parm outside correct boundary.'
         
-    if (float(values['assumedLat'].split('d')[1]) < 0):
+    if (float(values['assumedLat'].split('d')[1]) < 0 or float(values['assumedLat'].split('d')[1]) > 59):
         values['error']= 'assumedLat parm outside correct boundary.'
         
     
