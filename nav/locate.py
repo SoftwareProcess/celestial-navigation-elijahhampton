61a46f7e3a7df2211311d7b1f01978141bf72d4c
elijahhampton <ejh0017@gmail.com>
2019-04-29 12:08:59 -0700

    Refactor |

--- nav/locate.py
+++ nav/locate.py
@@ -9,6 +9,15 @@ def locate(values = None):
         values['error'] = 'Found parm with wrong type (correct: integer).'
         return values
     
+    try:
+        tempValues = values
+        for key in tempValues:
+            if (key == 'assumedLat'):
+                        float(tempValues[key].split('d')[1])
+    except ValueError:
+        values['error'] = 'Found parm with y.y portion as incorrect type. (correct: float)'
+        return values
+    
     if (int(values['assumedLat'].split('d')[0]) < -90 or int(values['assumedLat'].split('d')[0]) > 90):
         values['error']= 'assumedLat parm outside correct boundary.'
         
