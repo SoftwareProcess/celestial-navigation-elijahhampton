7e524ef96f9dedb8b550aeabbe5fb97de1efa1c5
elijahhampton <ejh0017@gmail.com>
2019-04-29 12:37:49 -0700

    Red Light |

--- nav/test/locateTest.py
+++ nav/test/locateTest.py
@@ -203,16 +203,3 @@ class locateTest(unittest.TestCase):
          
         self.assertEqual(locate(self.inputDictionary), tempResultDict)
         
-    def test200_010ShouldReturnWithErrorKeyIfCorrectedDistanceIsNegative(self):
-        self.setParm('assumedLat', '-53d38.4')
-        self.setParm('assumedLong', '350d35.3')
-        self.setParm('corrections', '[[-100,1d0.0]]')
-        self.setParm('op', 'locate')
-          
-        tempResultDict = {'op': 'locate',
-                          'assumedLat': '-53d38.4',
-                          'assumedLong': '350d35.3',
-                          'corrections': '[[-100,1d0.0]]',
-                          'error': 'Corrected Distance is Negative'}
-         
-        self.assertEqual(locate(self.inputDictionary), tempResultDict)
